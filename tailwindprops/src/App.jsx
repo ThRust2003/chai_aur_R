82c40fba2bcaef6c4c5cedd5b3d814d5ba63244a
ThRust2003 <rohanyadav10rbl@gmail.com>
2025-04-17 18:13:50 +0530

    changes

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './Card'
 
 function App() {
   const [count, setCount] = useState(0)
-
-  return (
+  
+    return (
     <>
       <div>
-       <h1 className='bg-green-600 text-black rounded-2xl'>green</h1>
+      
+      <Card channel="chaiwla" />
       </div>
        
     </>
--- tailwindprops/src/Card.jsx
+++ tailwindprops/src/Card.jsx
@@ -1,9 +1,10 @@
-function Card(){
-    return(
-        <>
-        <h1>
-            hello
-        </h1>
-        </>
-    )
+import React from 'react'
+
+function Card({channel,btntxt='8'}) {
+  return (
+    
+    <h1 className='bg-green-600 text-black rounded-2xl p-4'>Card{channel}{btntxt}</h1> //we could also use {btntxt||8}
+  )
 }
+
+export default Card
